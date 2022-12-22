f5b702975d05c83a422c6bad0a72f30c38d3e8ee
Fabian Correa <109480826+like53ar@users.noreply.github.com>
2022-12-22 20:14:23 -0300

    Create Buclefor.java

---
+++ buclefor/src/main/java/com/mycompany/buclefor/Buclefor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.buclefor;
+
+/**
+ *
+ * @author fabia
+ */
+public class Buclefor {
+
+    public static void main(String[] args) {
+       for (int cont=0;cont<=10;cont++){
+         /**
+          * codificar el for (inicializar contador, poner condicion de parada
+          * y sumar por cada paso)
+          */  
+           System.out.println("Estoy dando vueltas con for "+ cont);
+           cont =cont+1;
+           /**
+            * para evitar que siga en forma infinita
+            */
+       }
+    }
+}
